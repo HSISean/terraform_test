4b964d5182dabf6eb2618fa906e51eeaafa0a54d
hey_its_sean <sean.haughtonscott@gmail.com>
2026-09-15 18:24:58 -0400

    updated private ip cidr

--- main.tf
+++ main.tf
@@ -34,8 +34,8 @@ module "vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24"]
+  private_subnets = ["10.0.1.0/16", "10.0.2.0/16"]
+  public_subnets  = ["10.0.101.0/16"]
 
   enable_dns_hostnames = true
 }
